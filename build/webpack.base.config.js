5c5d8c4ffb117fbe173e07a0f70eda67ec995795
mttcug <mttcug@163.com>
2020-01-15 19:25:19 +0800

    Create webpack.base.config.js

---
+++ build/webpack.base.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './main.tsx')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[hash].js'
+    },
+    resolve: {
+        extensions: ['.tsx', '.jsx', '.js', '.html'],
+        alias: {
+            '@': './src',
+            '@page': './src/page',
+            '@router': './src/router',
+            '@component': './src/component',
+            '@static': './src/static'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(t|j)sx?$/,
+                use: ['babel-loader', 'tslint-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: "file-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: './index.html',
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
