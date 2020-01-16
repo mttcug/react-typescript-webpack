965afb828b9fb41e08839847746fd9b82e7fb9aa
mttcug <mttcug@163.com>
2020-01-16 17:47:20 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react'
 import { hot } from 'react-hot-loader/root'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './page/Home'
+import Errors from './page/Errors'
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
-                <Route path='/' component={ Home}/>
-                <Switch>
-                    <Route exact ></Route>
-                </Switch>
+                <Route path='/' exact component={ Home}/>
+                <Route path='/error' component={ Errors}/>
             </BrowserRouter>
         </div>
     )
