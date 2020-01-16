e75e977c8a087a79df558c28eacc5cf14ad47f06
mttcug <mttcug@163.com>
2020-01-16 17:10:53 +0800

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 180,
+    tabWidth: 4,
+    tabs: false,
+    semi: false,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    tailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    arrowParens: 'always',
+    htmlWhitespaceSensitivity: 'ignore',
+}
