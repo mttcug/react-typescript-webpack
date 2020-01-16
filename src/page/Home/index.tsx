7677ae35edeedb1ea26c694abcb15619065e68bc
mttcug <mttcug@163.com>
2020-01-16 17:46:38 +0800

    Create index.tsx

---
+++ src/page/Home/index.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react'
+// import { Switch, Route, NavLink } from 'react-router-dom'
+import { Layout, Menu, Icon } from 'antd'
+const { Header, Sider, Content } = Layout;
+
+const Home = () => {
+    return (
+        <Layout>
+        <Sider trigger={null} collapsible>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type='menu-unfold'
+            />
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              background: '#fff',
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    )
+}
+
+export default Home
