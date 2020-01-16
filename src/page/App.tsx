1e1beff560beabfadb3d33edf22559bb14800bda
mttcug <mttcug@163.com>
2020-01-16 19:41:48 +0800

    Create App.tsx

---
+++ src/page/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { hot } from 'react-hot-loader/root'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './Home'
+import Errors from './Errors'
+
+const App: React.SFC = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Route path='/' exact component={ Home}/>
+                <Route path='/error' component={ Errors}/>
+            </BrowserRouter>
+        </div>
+    )
+}
+export default hot(App)
