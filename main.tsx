2ad653eb6220a539c5a7a029b9933c1b440e7790
mttcug <mttcug@163.com>
2020-01-16 10:36:44 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,13 @@
 
 
+import 'react-hot-loader'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById("app"))
+const wrapper = document.getElementById("app")
+
+ReactDOM.render(
+    <App />,
+    wrapper
+)
