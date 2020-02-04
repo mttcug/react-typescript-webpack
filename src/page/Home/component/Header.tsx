d971f4ed56d57abd49ada5518095e79c95447611
LIUMIN HE <heliumin222@gmail.com>
2020-02-04 23:19:49 +0800

    test

--- src/page/Home/component/Header.tsx
+++ src/page/Home/component/Header.tsx
@@ -4,7 +4,7 @@ const { Header } = Layout
 
 const HeaderComponent: React.SFC = () => {
   return (
-    <Header style={{ background: '#fff', padding: 0 }}>
+    <Header style={{ background: '#fff', padding: '0px 20px' }}>
       <Icon
         className="trigger"
         type='menu-unfold'
